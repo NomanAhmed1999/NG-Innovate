03dbb0ccb84c5e0b0396a8a5ee741d0f43c38b5e
NomanAhmed1999 <nomanahmed0919@gmail.com>
2024-09-26 15:32:48 +0500

    upload js class 1

---
+++ Javascript/js-class-1/script.js
@@ -0,0 +1,166 @@
+// alert("Hello 2")
+// window.alert("Welcome to my websites")
+
+
+// Show msg on JS
+/**
+ * alert
+ * console.log
+ * document.write
+ */
+
+// console.log("I am on console"); // Cltr + I
+
+
+// document.write("<h1>I am Document writer</h1>")
+// document.write("I am Document writer")
+// document.write("<a href='#'>Go to google</a>")
+
+
+// Js is case sensitive
+// Koi change nhi krna jesa bola wese hi kru
+// Alert() // not work
+// aleRt() // not work
+// alert("Welcome")
+
+
+// Variables
+
+// dynamic values
+// var myName = "M. Bilal"
+
+// alert(myName)
+// console.log(myName);
+// document.write(myName)
+
+// Static values - hard coded
+// alert("M.Bilal")
+// console.log("M.Bilal");
+// document.write("M.Bilal")
+
+
+// Variables for String
+/**
+ * String
+ * double quotes
+ * single quotes
+ */
+
+// var myName = "Muhammad Bilal"
+// var institute = 'NG Innovate'
+
+// alert(myName)
+// alert(institute)
+
+
+// var quotes = "Quaid e Azam Said, 'Work,Work and Work'" 
+// alert(quotes)
+// console.log(quotes);
+
+// var quotes2 = 'NG Innovate Mission : "To provide high qualty tech knowladge"'
+
+// double -> single
+// single -> double
+
+// console.log(quotes2);
+
+// variable
+
+// var myName = "Muhammad Bilal"
+// alert("myName") // myName
+// alert(myName) // Muhammad Bilal
+
+
+// variables for Numbers
+// var age = "21"
+// alert(age)
+
+// console.log(4+2); // = 6
+
+// console.log("4"+"2"); // = CONCATENATION
+
+
+// var num1 = 5
+// var num2 = 6
+// console.log(num1 + num2); // 11
+//             5 + 6 = 11
+
+// console.log(typeof num1);
+
+
+// var name = "Bilal"
+// console.log(typeof name); // to find the type of the data
+
+
+// ----------> Rules for naming variables
+
+// comment / single line comment
+/**
+ * Multiline 
+ * Comment
+ */
+
+/**
+ * Legal
+ * can contain number, alphabets, underscore(_) and dollar sign $
+ * 
+ * Illegal
+ * can not used keywords
+ * whitespace is not allowed
+ * can not start from numbers
+ */
+
+// var my Name = "Muhammad Bilal"// whitespace
+// console.log(my Name);
+
+// var my_name = "Bilal" 
+// console.log(my_name);
+
+
+// var $isFound = true
+// console.log($isFound);
+
+// relateable
+// var _ = 10
+// console.log(_);
+
+
+// var 1Name = "Ali"
+// console.log(1Name);
+
+
+// var name1 = "Bilal"
+// var name2 = "Noman"
+
+// console.log(name1,name2);
+
+// reserved keyword
+// let class = "Muhammad bilal"
+// console.log(class);
+
+
+// variables also case sensitive
+
+// var myname = "Ali"
+// // console.log(MYNAME);
+// console.log(myname);
+
+
+// good practice
+// camelCase
+// full name
+// fullName -- > camelCase
+
+// var firstName = "Muhammad "
+// var lastName = "Bilal"
+// console.log(firstName + lastName);
+
+
+
+
+
+
+
+
+
+
---
+++ Javascript/js-class-1/script2.js
@@ -0,0 +1,82 @@
+// alert("Hello")
+
+
+// chapter 9 - Prompts - > for user input
+
+// var userName = prompt("What is your name?")
+// alert(userName)
+
+// concatenation -> multiples string ko merge krta he +
+
+// alert("user name is "+ userName)
+// document.write("user name is "+ userName)
+
+// document.write("<h1>My name is Bilal</h1>")
+// console.log(userName);
+
+// document.write("<h1>user name is : " + userName + "</h1>")
+
+
+// operators
+// arithmetic operators
+// + - * / % **
+
+
+// var num1 = 20
+// var num2 = 5
+// console.log("addition : ", num1 + num2);
+// console.log("subtraction : ", num1 - num2);
+// console.log("multiplication : ", num1 * num2);
+// console.log("division : ", num1 / num2);
+
+// console.log("division : ", 7 / 3 );
+// console.log("modulus / remainder : ", 7 % 7 );
+
+// console.log("Exponentional / power : ", 3**3 );
+
+// complex equation
+
+// console.log(25 - 5 * 10 / 5 ); // 15, 
+//          20 - 5 
+//               100 * 10
+//                      1000 / 5
+//                                 40
+
+
+// operator precedence
+// js -> topp to bottom
+// js -> left to right
+
+//  operator precedence
+// * , / -> first
+// +, - -> after
+
+// console.log((25 - 5) * (10 / 5) ); // 40, 
+// 20 * 2 => 40
+
+// console.log((((25 - 5) * 10) / 5) ); // 40, 
+
+
+// calculator
+// prompt always return string
+
+var num1 = +prompt("Enter number 1") // -> number
+var num2 = +prompt("Enter number 2")
+
+console.log(typeof num1);
+
+alert("addition " + (num1 + num2))
+
+
+// assignment: complete the calculator
+/**
+ * addition
+ * subtraction
+ * multiplication
+ * division
+ * modulus
+ * power
+ */
+
+
+
