f6a17e94277cb0f460764de9e5be99723cd8a7f6
Muhammad Bilal <mbilal254.m@gmail.com>
2024-09-30 10:48:43 -0700

    add js class 4

---
+++ Javascript/js-class-4/script.js
@@ -0,0 +1,92 @@
+// alert("hello")
+
+
+
+// var a = 1;
+
+// a = a++ + ++a * --a - a--;
+//     // 1   + 3  *   2 - 2
+//     //       (3*2) = 6
+//     //      1 + 6 = 7 
+//     //     7 - 2 = 5
+// console.log(a);
+
+
+
+// Chapter 10 
+// Conditional statements
+
+
+// booleans
+// true or false
+
+/** Conditional Operators always return boolean
+ * 
+ * equals ==
+ * greater than >
+ * less than <
+ * not equals !=
+ * 
+ * greater than equals to
+ * less than equals to
+ * 
+ * 2 specials
+ * === equals to with type checking
+ * !== not equals to with type checking
+ *  */ 
+
+// = assignment operators
+// == equals opeartors
+
+// basics
+// console.log( 5 == 4); // false
+// console.log( 5 > 4); // true
+// console.log( 5 < 4); // false
+// console.log( 5 != 4); // true
+
+
+// console.log( 10 >= 10); // true
+
+// 10 greater 10 -> false
+// or
+// 10 equls to 10 -> true 
+
+// console.log( 10 >= 9); // true
+
+// 10 greater 9 -> true => ab me aghe nhii dekhu ga
+// or 
+// 10 equls to 9 -> false
+
+// console.log( 10 >= 50); // false 
+
+
+// console.log(12 <= 10); // false
+
+// console.log(12 <= 12); // true
+
+// console.log(12 <= 30); //true
+
+
+// not equals
+// console.log(10 != 10); // false 
+// console.log(10 != 20); // true 
+
+// Special wale
+// type == data type
+// string, number, booleans
+
+// == -> ye bolta he mujhe farq nhi prta type jo bhi hu
+// console.log( 2 == "2"); // true why
+
+// === -> mujhe farq prta he type se
+// console.log(2 === "2"); // false
+
+
+// !== or !=
+console.log(5 != "6"); // true
+
+console.log(5 !== "4"); // true -> na type match he or na hi value true he
+
+console.log(5 !== 5); // false
+
+
---
+++ Javascript/js-class-4/script2.js
@@ -0,0 +1,64 @@
+// alert("Script 2")
+
+
+// If Conditions
+// agar ya magar
+
+// syntax
+// if(false){
+//     console.log("ab me chalo ga");
+// }
+
+// if or else
+// if(false){
+//     console.log("me tu ab bhi chalo ga");
+// }else{
+//     console.log("false me chalo ga");
+// }
+
+// Scenerio 🤔
+var samaan = +prompt("Saman k paise: ")
+var samaanAya = +prompt("Saman total amount: ") 
+
+// if(samaan > samaanAya ){
+//     console.log((samaan - samaanAya) +  " baqi ap rkh lena");
+// }
+// else{
+//     console.log("apka naseeb 😥");    
+// }
+
+// if(samaan > samaanAya){
+//     console.log((samaan - samaanAya) +  " baqi ap rkh lena");
+// }
+
+// if (samaan < samaanAya) {
+//     console.log("baqi mujhe milane pre");
+// }
+
+
+// if(true){
+//     console.log("1 me chalo ga");
+// }
+// if(true){
+//     console.log("2 me bhi chalo ga");
+// }
+
+
+// if else if
+
+// if (false) {
+//     console.log("me chalo ga");
+// }else if(true){
+//     console.log("ya tu me chalo ga");
+// }
+
+// next version
+
+if(samaan > samaanAya){
+    console.log((samaan - samaanAya) +  " baqi ap rkh lena");
+}else if(samaan < samaanAya){
+    console.log((samaanAya - samaan) + " baqi mujhe milane pre");
+}
+else{
+    console.log("apka naseeb 😥"); 
+}
